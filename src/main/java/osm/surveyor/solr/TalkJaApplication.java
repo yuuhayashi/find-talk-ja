5876f450983552c170b4e0b08049f271a4343b95
haya4 <hayashi.yuu@gmail.com>
2021-06-06 12:55:30 +0900

    WAR ファイル化

--- src/main/java/osm/surveyor/solr/TalkJaApplication.java
+++ src/main/java/osm/surveyor/solr/TalkJaApplication.java
@@ -2,12 +2,28 @@ package osm.surveyor.solr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TalkJaApplication {
+public class TalkJaApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TalkJaApplication.class, args);
 	}
 
+    /**
+     * Servletコンテナ起動時の設定クラス認識。
+     *
+     * <PRE>
+     * Servletコンテナで起動したときにどのクラスが設定クラスなのか認識させます。
+     * </PRE>
+     *
+     * @param  SpringApplicationBuilder
+     * @return SpringApplicationBuilder
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TalkJaApplication.class);
+    }
 }
