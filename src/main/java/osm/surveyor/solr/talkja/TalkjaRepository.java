3903f2927a0fb147de50dfc26dc04dab5af5dfbf
haya4 <hayashi.yuu@gmail.com>
2021-06-06 11:06:42 +0900

    contents

--- src/main/java/osm/surveyor/solr/talkja/TalkjaRepository.java
+++ src/main/java/osm/surveyor/solr/talkja/TalkjaRepository.java
@@ -36,7 +36,23 @@ public class TalkjaRepository {
 				summary.setId((String)document.get("id"));
 				summary.setName((String)document.get("name"));
 				summary.setDate((String)document.get("date"));
-				summary.setContents((List<String>)document.get("contents"));
+				
+				List<String> contents = (List<String>)document.get("contents");
+				List<String> conts6 = new ArrayList<>();
+				if (contents != null) {
+					int i = 0;
+					for (String str : contents) {
+						i++;
+						if (i <= 6) {
+							conts6.add(str);
+						}
+					}
+				}
+				else {
+					conts6.add("-");
+				}
+				summary.setContents(conts6);
+
 				list.add(summary);
 			}
 		} catch (SolrServerException e) {
